b575bfe780195bab0cd3ce89f429561a3cd09286
Krishna Dhakal <7krishna7dhakal7@gmail.com>
2023-08-09 18:03:11 +0545

    ccxt: average coin price api

--- src/binance/binance.controller.ts
+++ src/binance/binance.controller.ts
@@ -1,5 +1,5 @@
-import { Controller, Get } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import { Controller, Get, Param, Query } from '@nestjs/common';
+import { ApiParam, ApiTags } from '@nestjs/swagger';
 import { BinanceService } from './binance.service';
 
 @ApiTags('Binance')
@@ -7,8 +7,29 @@ import { BinanceService } from './binance.service';
 export class BinanceController {
   constructor(private readonly binanceService: BinanceService) {}
 
-  @Get('tradable-coins')
+  @Get('tradableCoins')
   async getTradableCoins(): Promise<string[]> {
     return this.binanceService.getTradableCoins();
+  }
+
+  @ApiParam({
+    name: 'baseCoin',
+    description:
+      'Base coin whose pair prices needs to be calculated. Eg: USDT, USDC',
+  })
+  @ApiParam({
+    name: 'calculateAveragePrices',
+    description: 'Boolean value to calculate average prices',
+  })
+  @Get('coinPrice/:baseCoin/:calculateAveragePrices')
+  async getCoinData(
+    @Param('baseCoin') baseCoin: string,
+    @Param('calculateAveragePrices') calculateAveragePrices: string,
+  ): Promise<any> {
+    const shouldCalculateAveragePrices = calculateAveragePrices === 'true';
+    return this.binanceService.getCoinData(
+      baseCoin,
+      shouldCalculateAveragePrices,
+    );
   }
 }
--- src/binance/binance.service.ts
+++ src/binance/binance.service.ts
@@ -19,4 +19,79 @@ export class BinanceService {
       throw new Error('Failed to fetch tradable coins.');
     }
   }
+
+  async getSortedCoinPairs(baseCoin: string): Promise<string[]> {
+    try {
+      const exchange = new ccxt.binance();
+      const markets = await exchange.loadMarkets();
+
+      const sortedCoinPairs = Object.keys(markets)
+        .filter((symbol) => {
+          const [base, quote] = symbol.split('/');
+          return quote === baseCoin;
+        })
+        .sort();
+
+      console.log(sortedCoinPairs);
+
+      return sortedCoinPairs;
+    } catch (error) {
+      console.error(
+        'Error fetching sorted coin pairs from Binance:',
+        error.message,
+      );
+      throw new Error('Failed to fetch sorted coin pairs.');
+    }
+  }
+
+  async getCoinPairAveragePrice(pair: string): Promise<number> {
+    try {
+      const exchange = new ccxt.binance();
+      const recentTrades = await exchange.fetchTrades(pair, undefined, 100);
+      const lastHundredPrices = recentTrades.map((trade) => trade.price);
+
+      if (lastHundredPrices.length > 0) {
+        const averagePrice =
+          lastHundredPrices.reduce((sum, price) => sum + price, 0) /
+          lastHundredPrices.length;
+        console.log(averagePrice);
+        return averagePrice;
+      } else {
+        return 0; // No recent trades found
+      }
+    } catch (error) {
+      console.error(
+        'Error fetching recent trades for coin pair:',
+        pair,
+        error.message,
+      );
+      throw new Error('Failed to fetch recent trades for coin pair.');
+    }
+  }
+
+  async getCoinData(
+    baseCoin: string,
+    calculateAveragePrices: boolean,
+  ): Promise<any> {
+    try {
+      const sortedCoinPairs = await this.getSortedCoinPairs(baseCoin);
+      const response: any = { sortedCoinPairs, averagePrices: {} };
+
+      if (calculateAveragePrices) {
+        const averagePricePromises: Promise<void>[] = sortedCoinPairs.map(
+          async (coinPair) => {
+            const averagePrice = await this.getCoinPairAveragePrice(coinPair);
+            response.averagePrices[coinPair] = averagePrice;
+          },
+        );
+
+        await Promise.all(averagePricePromises);
+      }
+
+      return response;
+    } catch (error) {
+      console.error('Error fetching coin data:', error.message);
+      throw new Error('Failed to fetch coin data.');
+    }
+  }
 }
