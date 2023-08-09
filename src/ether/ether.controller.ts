fb391edee20b814626440a777e7336f8dbbedcfb
Krishna Dhakal <7krishna7dhakal7@gmail.com>
2023-08-09 15:17:48 +0545

    initiate swagger

--- src/ether/ether.controller.ts
+++ src/ether/ether.controller.ts
@@ -1,8 +1,14 @@
 import { Controller, Get, Param, NotFoundException } from '@nestjs/common';
 import { ethers } from 'ethers';
+import { ApiParam, ApiTags } from '@nestjs/swagger'; // Import Swagger decorators
 
+@ApiTags('Ether') // Use ApiTags decorator to specify controller tag
 @Controller('ether')
 export class EtherController {
+  @ApiParam({
+    name: 'address',
+    description: 'Ethereum address to be validated',
+  }) // Use ApiParam decorator for parameter description
   @Get('validate/:address')
   validateAddress(@Param('address') address: string): string {
     if (!ethers.isAddress(address)) {
